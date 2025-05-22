3bd4203dc0490af34d80335b27bdfdd70fa9385e
Leh-02 <ivalyash51@gmail.com>
2025-05-22 18:23:51 +0300

    trying to fix bugs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,17 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
+  tabs.map(tab => (
+    <li
+      className={tab.id === activeTabId && "is-active"}
+      data-cy="Tab"
+      onClick={(event) => { 
+        if (tab.id !== activeTabId) { 
+          onTabSelected(event.target.id);
+        }
+      }}
+    >
+        <a href={`#${tab.id}`} data-cy="TabLink">
+          {tab.title}
+        </a>
+      </li>
+    ))
+);
